16b57ce8b4bcd59dcbebbfea5fff3cca8f438627
EatNug <wlq4568@gmail.com>
2019-07-04 15:42:54 +0900

    added follow

--- nomadgram/users/urls.py
+++ nomadgram/users/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = "users"
 urlpatterns = [
-    path('explore/', views.ExploreUser.as_view(), name= 'explore_users')
+    path('explore/', views.ExploreUser.as_view(), name= 'explore_users'),
+    path('<int:user_id>/follow/', views.FollowUser.as_view(), name='follow_user'),
 ]
