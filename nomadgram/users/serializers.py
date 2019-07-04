4e2e28ce637b07ff5cf8283a6295112899cc8685
EatNug <wlq4568@gmail.com>
2019-07-04 15:42:38 +0900

    added followings & followers to fields and applied FeedUserSerializer

--- nomadgram/users/serializers.py
+++ nomadgram/users/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
 from . import models
+from nomadgram.images import serializers as image_serializer
 
 class ExploreUserSerializer(serializers.ModelSerializer):
+
+    followings = image_serializer.FeedUserSerializer(many=True)
+    followers = image_serializer.FeedUserSerializer(many=True)
 
     class Meta:
         model = models.User
         fields = (
+            'id',
             'profile_image',
             'username',
-            'name'
+            'name',
+            'followings',
+            'followers'
         )
